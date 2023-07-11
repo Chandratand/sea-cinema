035e0966697997143552fad4ed662ff311f0c963
Chandra Tandiono <90386494+Chandratand@users.noreply.github.com>
2023-07-11 10:38:37 +0700

    add redirect to sign up button in sign in page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 import LoginImage from "../../../public/images/login-image.jpg";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import SignInForm from "./SignInForm";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const SignIn = async () => {
   const session = await getServerSession(authOptions);
@@ -14,6 +17,15 @@ const SignIn = async () => {
 
   return (
     <div className="container relative grid h-screen flex-col items-center justify-center md:max-w-none md:grid-cols-2 md:px-0">
+      <Link
+        href="/sign-up"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute right-4 top-4 md:right-8 md:top-8"
+        )}
+      >
+        Sign Up
+      </Link>
       <div className="relative hidden h-full flex-col justify-end bg-muted p-10 text-white dark:border-r md:flex">
         <div className="absolute inset-0 bg-zinc-900 object-contain">
           <Image
